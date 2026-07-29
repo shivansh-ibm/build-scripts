8b464711395806eee6491030d1798961ec3a8080
shivansh-ibm <shivansh.s1@ibm.com>
2026-07-29 01:30:26 -0700

    Added build script for cytoolz==1.0.1 for UBI10.2
    Updated build script for python 3.14

---
+++ c/cytoolz/cytoolz_1.1.0_ubi_10.2.sh
@@ -0,0 +1,90 @@
+#!/bin/bash -e
+# -----------------------------------------------------------------------------
+#
+# Package          : cytoolz
+# Version          : 1.1.0
+# Source repo      : https://github.com/pytoolz/cytoolz.git
+# Tested on        : UBI:10.2
+# Language         : Python
+# Ci-Check         : True
+# Script License   : Apache License, Version 2 or later
+# Maintainer       : Shivansh Sharma <Shivansh.S1@ibm.com>
+#
+# Disclaimer: This script has been tested in root mode on given
+# ==========  platform using the mentioned version of the package.
+#             It may not work as expected with newer versions of the
+#             package and/or distribution. In such case, please
+#             contact "Maintainer" of this script.
+#
+# -----------------------------------------------------------------------------
+
+set -e
+
+PACKAGE_NAME=cytoolz
+PACKAGE_VERSION=${1:-1.1.0}
+PACKAGE_URL=https://github.com/pytoolz/cytoolz.git
+PACKAGE_DIR=cytoolz
+CURRENT_DIR=$(pwd)
+
+# Install system dependencies (Python packages must come first)
+yum install -y python3.14 python3.14-devel python3.14-pip \
+    git gcc-toolset-15 gcc-toolset-15-gcc gcc-toolset-15-gcc-c++ \
+    make cmake wget openssl-devel bzip2-devel libffi-devel zlib-devel
+
+if [[ -f /opt/rh/gcc-toolset-15/enable ]]; then
+    source /opt/rh/gcc-toolset-15/enable
+elif [[ -d /opt/rh/gcc-toolset-15/root/usr/bin ]]; then
+    export PATH="/opt/rh/gcc-toolset-15/root/usr/bin:$PATH"
+    export LD_LIBRARY_PATH="/opt/rh/gcc-toolset-15/root/usr/lib64:$LD_LIBRARY_PATH"
+else
+    echo "ERROR: gcc-toolset-15 not found"
+    exit 1
+fi
+
+echo "Using gcc: $(gcc --version | head -1)"
+
+python3.14 -m pip install --upgrade pip setuptools wheel build
+python3.14 -m pip install Cython pytest setuptools-git-versioning
+python3.14 -m pip install git+https://github.com/pytoolz/toolz.git#egg=toolz
+
+# Clone the repository
+cd "$CURRENT_DIR"
+git clone "$PACKAGE_URL" "$PACKAGE_DIR"
+cd "$PACKAGE_DIR"
+
+if git rev-parse "v${PACKAGE_VERSION}" &>/dev/null; then
+    git checkout "v${PACKAGE_VERSION}"
+elif git rev-parse "${PACKAGE_VERSION}" &>/dev/null; then
+    git checkout "${PACKAGE_VERSION}"
+else
+    echo "ERROR: No git tag found for version '${PACKAGE_VERSION}'"
+    exit 1
+fi
+
+# Install
+if ! python3.14 -m pip install --no-build-isolation .; then
+    echo "------------------$PACKAGE_NAME:Install_fails-------------------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_Fails"
+    exit 1
+fi
+
+# Build wheel
+python3.14 -m build --wheel --no-isolation --outdir="$CURRENT_DIR/"
+
+# Run tests
+cd "$CURRENT_DIR"
+mkdir /tmp/cytoolz_test
+cp -r $CURRENT_DIR/$PACKAGE_DIR/cytoolz/ /tmp/cytoolz_test/
+
+if ! pytest "/tmp/cytoolz_test/"; then
+    echo "------------------$PACKAGE_NAME:Install_success_but_test_fails---------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub | Fail |  Install_success_but_test_Fails"
+    exit 2
+else
+    echo "------------------$PACKAGE_NAME:Install_&_test_both_success-------------------------"
+    echo "$PACKAGE_URL $PACKAGE_NAME"
+    echo "$PACKAGE_NAME  |  $PACKAGE_URL | $PACKAGE_VERSION | GitHub  | Pass |  Both_Install_and_Test_Success"
+    exit 0
+fi
